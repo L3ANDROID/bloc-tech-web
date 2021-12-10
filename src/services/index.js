212127a157eb55d54707d190f3b530ccbf0febf4
Leandro Gabriel <40245178+L3ANDROID@users.noreply.github.com>
2021-12-10 15:54:09 -0500

    b

--- src/services/index.js
+++ src/services/index.js
@@ -1,2 +1,2 @@
-export const BASE_URL = "http://3cf0-45-5-68-107.ngrok.io/api/v1";
-// export const BASE_URL = "http://localhost:8080/api/v1";
+// export const BASE_URL = "http://3cf0-45-5-68-107.ngrok.io/api/v1";
+export const BASE_URL = "http://localhost:8080/api/v1";
